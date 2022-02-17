b9013d727434dca501af310e5e6e536e7bfef1b6
CharlesK <xk2144@columbia.edu>
2022-02-17 13:05:57 -0500

    init commit

---
+++ src/main/java/com/columbia/iotabacserver/IotabacserverApplication.java
@@ -0,0 +1,13 @@
+package com.columbia.iotabacserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotabacserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotabacserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/columbia/iotabacserver/IotabacserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.columbia.iotabacserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotabacserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
