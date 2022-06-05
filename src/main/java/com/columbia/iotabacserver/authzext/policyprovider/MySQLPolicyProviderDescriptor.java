6aac68df03a81c36dd604de7320cd3964c151a32
CharlesK <xk2144@columbia.edu>
2022-06-04 20:28:19 -0700

    merge opa into main

--- src/main/java/com/columbia/iotabacserver/authzext/policyprovider/MySQLPolicyProviderDescriptor.java
+++
@@ -1,7 +0,0 @@
-//package com.columbia.iotabacserver.authzext.policyprovider;
-//
-//import org.ow2.authzforce.xmlns.pdp.ext.AbstractPolicyProvider;
-//
-//public class MySQLPolicyProviderDescriptor extends AbstractPolicyProvider {
-//
-//}
--- src/main/java/com/columbia/iotabacserver/controller/AbacAuthenticationController.java
+++
@@ -1,8 +0,0 @@
-package com.columbia.iotabacserver.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class AbacAuthenticationController {
-
-}
